2ce7a34813db03ae5e805ee7a5d38169459acf5e
terakado <sakusaku.sasuke0426@gmail.com>
2020-06-16 14:33:27 +0900

    analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  GA.tracker = "UA-169632115-1"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
