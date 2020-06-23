6540e2f5eeec0ab49f6e17ce06ef7c4add601720
terakado <sakusaku.sasuke0426@gmail.com>
2020-06-24 02:54:44 +0900

    js

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,16 +1,48 @@
 $(document).on('turbolinks:load',function(){
-  $('.image__s').hover(function(){
+  $('.image__s1').hover(function(){
     var target = $(this);
     setHover= setTimeout(function(){
-      $('.active-img').removeClass('active-img');
-      $(target).addClass('active-img');
+      $('.active-img_1').removeClass('active-img_1');
+      $(target).addClass('active-img_1');
       var parent = target.parent().parent()
-      var target2 = parent.find('.card--image');
-      var id = $('.active-img').data("id");
+      var target2 = parent.find('#card--image_1');
+      var id = $('.active-img_1').data("id");
       var width = $('.image').width();
     
       $(target2).animate({"right": id * width + "px", easing:'easeInOutExpo'}); 
-    },700)
+    },500)
+  },function(){
+    clearTimeout(setHover)
+  });
+
+  $('.image__s2').hover(function(){
+    var target = $(this);
+    setHover= setTimeout(function(){
+      $('.active-img_2').removeClass('active-img_2');
+      $(target).addClass('active-img_2');
+      var parent = target.parent().parent()
+      var target2 = parent.find('#card--image_2');
+      var id = $('.active-img_2').data("id");
+      var width = $('.image').width();
+    
+      $(target2).animate({"right": id * width + "px", easing:'easeInOutExpo'}); 
+    },500)
+  },function(){
+    clearTimeout(setHover)
+  });
+
+  $('.image__s3').hover(function(){
+    var target = $(this);
+    setHover= setTimeout(function(){
+      $('.active-img_3').removeClass('active-img_3');
+      $(target).addClass('active-img_3');
+      var parent = target.parent().parent()
+      var target2 = parent.find('#card--image_3');
+      var id = $('.active-img_3').data("id");
+      var width = $('.image').width();
+    
+      $(target2).animate({"right": id * width + "px", easing:'easeInOutExpo'}); 
+    },500)
   },function(){
     clearTimeout(setHover)
   });
